8ad550cfd4384343e30bbee20599386cdc498728
egorca_05 <gapeevegor@gmail.com>
2022-04-26 23:05:58 +0300

    Авторизация + папки + база + MBClass

--- BetBoom/WindowFolder/NoRoleWindow/AutorizationWindow.xaml.cs
+++ BetBoom/WindowFolder/NoRoleWindow/AutorizationWindow.xaml.cs
@@ -1,16 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using BetBoom.ClassFolder;
+using BetBoom.DataFolder;
+using BetBoom.WindowFolder.AdminFolder;
+using BetBoom.WindowFolder.UserFolder;
+using System;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace BetBoom.WindowFolder.NoRoleWindow
 {
@@ -22,6 +16,69 @@ namespace BetBoom.WindowFolder.NoRoleWindow
         public AutorizationWindow()
         {
             InitializeComponent();
+        }
+
+        private void InBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(LoginTb.Text))
+            {
+                MBClass.MBError("Введите логин");
+                LoginTb.Focus();
+            }
+            if (string.IsNullOrEmpty(PasswordPB.Password))
+            {
+                MBClass.MBError("Введите пароль");
+                PasswordPB.Focus();
+            }
+            else
+            {
+                try
+                {
+                    var user = DBEntities.GetContext().User.FirstOrDefault
+                        (u => u.login == LoginTb.Text);
+                    if (user == null)
+                    {
+                        MBClass.MBError("Пользователь не найден");
+                        LoginTb.Focus();
+                        return;
+                    }
+                    if (user.Password != PasswordPB.Password)
+                    {
+                        MBClass.MBError("Введен не правильный пароль");
+                        PasswordPB.Focus();
+                        return;
+                    }
+                    else
+                    {
+                        //context.user = user; возможно не пригодиться
+                        switch (user.IdRole)
+                        {
+                            case 1:
+                                AdminMatchWindow adminMatchWindow = new AdminMatchWindow();
+                                adminMatchWindow.Show();
+                                this.Close();
+                                break;
+                            case 2:
+                                UserMatchWindow userMatchWindow =
+                                    new UserMatchWindow();
+                                userMatchWindow.Show();
+                                this.Close();
+                                break;
+                            /*case 3:
+                                DirectorFolder.DirectorMapWindow
+                                    directorMapWindow =
+                                    new DirectorFolder.DirectorMapWindow();
+                                directorMapWindow.Show();
+                                this.Close();
+                                break; */
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MBClass.MBError(ex);
+                }
+            }
         }
     }
 }
