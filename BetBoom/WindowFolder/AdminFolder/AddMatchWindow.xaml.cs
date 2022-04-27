3c73ffe5aa2db89065542ef4f81c16d7444b0e1a
egorca_05 <gapeevegor@gmail.com>
2022-04-27 20:19:26 +0300

    Окно добавления матча готово

---
+++ BetBoom/WindowFolder/AdminFolder/AddMatchWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using BetBoom.DataFolder;
+using BetBoom.ClassFolder;
+
+namespace BetBoom.WindowFolder.AdminFolder
+{
+    /// <summary>
+    /// Логика взаимодействия для AddMatchWindow.xaml
+    /// </summary>
+    public partial class AddMatchWindow : Window
+    {
+        public AddMatchWindow()
+        {
+            InitializeComponent();
+            Team1CB.ItemsSource = DBEntities.GetContext()
+                .TeamOne.ToList();
+            Team2CB.ItemsSource = DBEntities.GetContext()
+                .TeamTwo.ToList();
+            SportCB.ItemsSource = DBEntities.GetContext()
+                .Sport.ToList();
+        }
+
+        private void ExitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void RegistrationBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (Team1CB.SelectedValue.ToString() == Team2CB.SelectedValue.ToString())
+            {
+                MBClass.MBError("Команды не могут совпадать");
+            }
+            else
+            {
+                AddMatch();
+                MBClass.MBInformation("Матч успешно создан");
+                this.Close();
+            }
+        }
+
+        private void AddMatch()
+        {
+            DBEntities.GetContext().Match.Add(new Match()
+            {
+                IdTeamOne = Int32.Parse(Team1CB.SelectedValue.ToString()),
+                IdTeamTwo = Int32.Parse(Team2CB.SelectedValue.ToString()),
+                IdSport = Int32.Parse(SportCB.SelectedValue.ToString()),
+                Coefficient = Int32.Parse(KofTB.Text),              
+            }); ;
+            DBEntities.GetContext().SaveChanges();
+        }
+    }
+}
--- BetBoom/WindowFolder/AdminFolder/AdminMatchWindow.xaml.cs
+++ BetBoom/WindowFolder/AdminFolder/AdminMatchWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using BetBoom.DataFolder;
+using BetBoom.ClassFolder;
+using BetBoom.WindowFolder.AdminFolder;
 
 namespace BetBoom.WindowFolder.AdminFolder
 {
@@ -25,17 +27,38 @@ namespace BetBoom.WindowFolder.AdminFolder
             InitializeComponent();
             MatchDG.ItemsSource = DBEntities.GetContext().Match.ToList().
                 OrderBy(c => c.IdMatch);
-            //var team2 = DBEntities.GetContext().Role
-            //    .FirstOrDefault(s => s.IdRole == User.IdRole);
-            //RoleTB.Text = role.NameRole;
-            //var sport = DBEntities.GetContext().Role
-            //    .FirstOrDefault(s => s.IdRole == User.IdRole);
-            //RoleTB.Text = role.NameRole;
         }
 
         private void UserDG_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
 
+        }
+
+        private void AddBtn_Click(object sender, RoutedEventArgs e)
+        {
+            AddMatchWindow addMatchWindow = new AddMatchWindow();
+            addMatchWindow.ShowDialog();
+            MatchDG.ItemsSource = DBEntities.GetContext().Match.ToList().
+               OrderBy(c => c.IdMatch);
+        }
+
+        private void DelBtn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Match match = MatchDG.SelectedItem as Match;
+                if (MBClass.QuestionMessage($"Удалить выбранный матч?"))
+                {
+                    DBEntities.GetContext().Match.Remove(match);
+                    DBEntities.GetContext().SaveChanges();
+                    MatchDG.ItemsSource = DBEntities.GetContext().Match.ToList().
+                OrderBy(c => c.IdMatch);
+                }
+            }
+            catch (Exception ex)
+            {
+                MBClass.MBError(ex);
+            }
         }
     }
 }
